db5a75bf858190051f4bbcf3d0585381ceba5f05
Luis Lopez <xuzo@cuarentaydos.com>
2008-09-22 08:07:33 +0000

    Renamed to EinaPlayer

--- eina/eina-player-volume.c
+++ eina/eina-player-volume.c
@@ -1,43 +1,43 @@
-#define GEL_DOMAIN "Eina::Player::Volume"
+#define GEL_DOMAIN "Eina::Volume"
 
-#include "eina-player-volume.h"
+#include "eina-volume.h"
 #include <gel/gel.h>
 
-G_DEFINE_TYPE (EinaPlayerVolume, eina_player_volume, GTK_TYPE_VOLUME_BUTTON)
+G_DEFINE_TYPE (EinaVolume, eina_volume, GTK_TYPE_VOLUME_BUTTON)
 
 #define GET_PRIVATE(o) \
-	(G_TYPE_INSTANCE_GET_PRIVATE ((o), EINA_PLAYER_TYPE_VOLUME, EinaPlayerVolumePrivate))
+	(G_TYPE_INSTANCE_GET_PRIVATE ((o), EINA_TYPE_VOLUME, EinaVolumePrivate))
 
-// #define EINA_PLAYER_VOLUME_DYNAMIC_UPDATE
+// #define EINA_VOLUME_DYNAMIC_UPDATE
 
-typedef struct _EinaPlayerVolumePrivate EinaPlayerVolumePrivate;
+typedef struct _EinaVolumePrivate EinaVolumePrivate;
 
-struct _EinaPlayerVolumePrivate
+struct _EinaVolumePrivate
 {
 	LomoPlayer *lomo;
 };
 
 enum {
-	EINA_PLAYER_VOLUME_LOMO_PLAYER_PROPERTY = 1
+	EINA_VOLUME_LOMO_PLAYER_PROPERTY = 1
 };
 
-#ifdef EINA_PLAYER_VOLUME_DYNAMIC_UPDATE
+#ifdef EINA_VOLUME_DYNAMIC_UPDATE
 static gboolean
-eina_player_volume_update(EinaPlayerVolume *self);a
+eina_volume_update(EinaVolume *self);a
 #endif
 
 static void
-on_eina_player_volume_value_changed (GtkWidget *w, gdouble value, EinaPlayerVolume *self);
+on_eina_volume_value_changed (GtkWidget *w, gdouble value, EinaVolume *self);
 
 static void
-eina_player_volume_get_property(GObject *object, guint property_id,
+eina_volume_get_property(GObject *object, guint property_id,
 	GValue *value, GParamSpec *pspec)
 {
-	EinaPlayerVolume *self = EINA_PLAYER_VOLUME(object);
+	EinaVolume *self = EINA_VOLUME(object);
 
 	switch (property_id) {
-	case EINA_PLAYER_VOLUME_LOMO_PLAYER_PROPERTY:
-		g_value_set_object(value, (gpointer) eina_player_volume_get_lomo_player(self));
+	case EINA_VOLUME_LOMO_PLAYER_PROPERTY:
+		g_value_set_object(value, (gpointer) eina_volume_get_lomo_player(self));
 		break;
 
 	default:
@@ -46,14 +46,14 @@ eina_player_volume_get_property(GObject *object, guint property_id,
 }
 
 static void
-eina_player_volume_set_property (GObject *object, guint property_id,
+eina_volume_set_property (GObject *object, guint property_id,
 	const GValue *value, GParamSpec *pspec)
 {
-	EinaPlayerVolume *self = EINA_PLAYER_VOLUME(object);
+	EinaVolume *self = EINA_VOLUME(object);
 
 	switch (property_id) {
-	case EINA_PLAYER_VOLUME_LOMO_PLAYER_PROPERTY:
-		eina_player_volume_set_lomo_player(self, LOMO_PLAYER(g_value_get_object(value)));
+	case EINA_VOLUME_LOMO_PLAYER_PROPERTY:
+		eina_volume_set_lomo_player(self, LOMO_PLAYER(g_value_get_object(value)));
 		break;
 
 	default:
@@ -62,19 +62,19 @@ eina_player_volume_set_property (GObject *object, guint property_id,
 }
 
 static void
-eina_player_volume_dispose (GObject *object)
+eina_volume_dispose (GObject *object)
 {
-	EinaPlayerVolume *self = EINA_PLAYER_VOLUME(object);
-	EinaPlayerVolumePrivate *priv = GET_PRIVATE(self);
+	EinaVolume *self = EINA_VOLUME(object);
+	EinaVolumePrivate *priv = GET_PRIVATE(self);
 
 	gel_free_and_invalidate(priv->lomo, NULL, g_object_unref);
 
-	if (G_OBJECT_CLASS (eina_player_volume_parent_class)->dispose)
-		G_OBJECT_CLASS (eina_player_volume_parent_class)->dispose (object);
+	if (G_OBJECT_CLASS (eina_volume_parent_class)->dispose)
+		G_OBJECT_CLASS (eina_volume_parent_class)->dispose (object);
 }
 
 static void
-eina_player_volume_class_init (EinaPlayerVolumeClass *klass)
+eina_volume_class_init (EinaVolumeClass *klass)
 {
 	GObjectClass *object_class = G_OBJECT_CLASS (klass);
 	GdkPixbuf *pb;
@@ -96,13 +96,13 @@ eina_player_volume_class_init (EinaPlayerVolumeClass *klass)
 		NULL
 	};
 
-	g_type_class_add_private (klass, sizeof (EinaPlayerVolumePrivate));
+	g_type_class_add_private (klass, sizeof (EinaVolumePrivate));
 
-	object_class->dispose = eina_player_volume_dispose;
-	object_class->get_property = eina_player_volume_get_property;
-	object_class->set_property = eina_player_volume_set_property;
+	object_class->dispose = eina_volume_dispose;
+	object_class->get_property = eina_volume_get_property;
+	object_class->set_property = eina_volume_set_property;
 
-	g_object_class_install_property(object_class, EINA_PLAYER_VOLUME_LOMO_PLAYER_PROPERTY,
+	g_object_class_install_property(object_class, EINA_VOLUME_LOMO_PLAYER_PROPERTY,
 		g_param_spec_object("lomo-player", "Lomo player", "Lomo Player to control/watch",
 		LOMO_TYPE_PLAYER, G_PARAM_READABLE | G_PARAM_WRITABLE | G_PARAM_CONSTRUCT));
 
@@ -124,7 +124,7 @@ eina_player_volume_class_init (EinaPlayerVolumeClass *klass)
 }
 
 static void
-eina_player_volume_init (EinaPlayerVolume *self)
+eina_volume_init (EinaVolume *self)
 {
 	static const gchar *icons[] = {
 		"audio-volume-mute",
@@ -134,25 +134,26 @@ eina_player_volume_init (EinaPlayerVolume *self)
 		NULL
 	};
 
+	gel_warn("I have problems with icons");
 	gtk_scale_button_set_icons(GTK_SCALE_BUTTON(self), icons);
 	g_signal_connect(self, "value-changed",
-		G_CALLBACK(on_eina_player_volume_value_changed), self);
-#ifdef EINA_PLAYER_VOLUME_DYNAMIC_UPDATE
-	eina_player_volume_update(self);
-	g_timeout_add(900, (GSourceFunc) eina_player_volume_update, (gpointer) self);
+		G_CALLBACK(on_eina_volume_value_changed), self);
+#ifdef EINA_VOLUME_DYNAMIC_UPDATE
+	eina_volume_update(self);
+	g_timeout_add(900, (GSourceFunc) eina_volume_update, (gpointer) self);
 #endif
 }
 
-EinaPlayerVolume*
-eina_player_volume_new (void)
+EinaVolume*
+eina_volume_new (void)
 {
-	return g_object_new (EINA_PLAYER_TYPE_VOLUME, NULL);
+	return g_object_new (EINA_TYPE_VOLUME, NULL);
 }
 
 void
-eina_player_volume_set_lomo_player(EinaPlayerVolume *self, LomoPlayer *lomo)
+eina_volume_set_lomo_player(EinaVolume *self, LomoPlayer *lomo)
 {
-	struct _EinaPlayerVolumePrivate *priv = GET_PRIVATE(self);
+	struct _EinaVolumePrivate *priv = GET_PRIVATE(self);
 
 	if (!LOMO_IS_PLAYER(lomo))
 		return;
@@ -165,23 +166,23 @@ eina_player_volume_set_lomo_player(EinaPlayerVolume *self, LomoPlayer *lomo)
 
 	gtk_scale_button_set_value(GTK_SCALE_BUTTON(self),
 		(gdouble) lomo_player_get_volume(lomo) / 100);
-#ifdef EINA_PLAYER_VOLUME_DYNAMIC_UPDATE
-	eina_player_volume_update(self);
+#ifdef EINA_VOLUME_DYNAMIC_UPDATE
+	eina_volume_update(self);
 #endif
 }
 
 LomoPlayer*
-eina_player_volume_get_lomo_player(EinaPlayerVolume *self)
+eina_volume_get_lomo_player(EinaVolume *self)
 {
-	struct _EinaPlayerVolumePrivate *priv = GET_PRIVATE(self);
+	struct _EinaVolumePrivate *priv = GET_PRIVATE(self);
 	return priv->lomo;
 }
 
-#ifdef EINA_PLAYER_VOLUME_DYNAMIC_UPDATE
+#ifdef EINA_VOLUME_DYNAMIC_UPDATE
 static gboolean
-eina_player_volume_update(EinaPlayerVolume *self)
+eina_volume_update(EinaVolume *self)
 {
-	struct _EinaPlayerVolumePrivate *priv = GET_PRIVATE(self);
+	struct _EinaVolumePrivate *priv = GET_PRIVATE(self);
 	gint value;
 	gdouble v;
 
@@ -197,9 +198,9 @@ eina_player_volume_update(EinaPlayerVolume *self)
 #endif
 
 static void
-on_eina_player_volume_value_changed (GtkWidget *w, gdouble value, EinaPlayerVolume *self)
+on_eina_volume_value_changed (GtkWidget *w, gdouble value, EinaVolume *self)
 {
-	EinaPlayerVolumePrivate *priv = GET_PRIVATE(self);
+	EinaVolumePrivate *priv = GET_PRIVATE(self);
 
 	if (priv->lomo)
 		lomo_player_set_volume(priv->lomo, (value * 100) / 1);
