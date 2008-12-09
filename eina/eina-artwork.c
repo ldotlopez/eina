df9b27a2677e943bfda2d9845bf28d354b0dc946
Luis Lopez <xuzo@cuarentaydos.com>
2008-12-09 08:02:59 +0000

    Rewrite of EinaCover

---
+++ eina/eina-artwork.c
@@ -0,0 +1,236 @@
+#define GEL_DOMAIN "Eina::Artwork"
+#include <gel/gel.h>
+#include <eina/eina-artwork.h>
+
+G_DEFINE_TYPE (EinaArtwork, eina_artwork, GTK_TYPE_IMAGE)
+
+#define GET_PRIVATE(o) \
+	(G_TYPE_INSTANCE_GET_PRIVATE ((o), EINA_TYPE_ARTWORK, EinaArtworkPrivate))
+
+typedef struct _EinaArtworkPrivate EinaArtworkPrivate;
+
+struct _EinaArtworkPrivate {
+	LomoStream *stream;
+	GList      *providers, *running;
+};
+
+struct _EinaArtworkProvider {
+	const gchar *name;
+	EinaArtworkProviderSearchFunc search;
+	EinaArtworkProviderCancelFunc cancel;
+	gpointer data;
+};
+
+enum {
+	EINA_ARTWORK_PROPERTY_STREAM = 1
+};
+
+static gboolean
+search_provider(EinaArtwork *self, const gchar *name);
+static void
+run_queue(EinaArtwork *self);
+
+static void
+eina_artwork_get_property (GObject *object, guint property_id,
+	GValue *value, GParamSpec *pspec)
+{
+	switch (property_id) {
+	case EINA_ARTWORK_PROPERTY_STREAM:
+		g_value_set_object(value, (gpointer) eina_artwork_get_stream((EinaArtwork *) object));
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+	}
+}
+
+static void
+eina_artwork_set_property (GObject *object, guint property_id,
+	const GValue *value, GParamSpec *pspec)
+{
+	switch (property_id) {
+	case EINA_ARTWORK_PROPERTY_STREAM:
+		eina_artwork_set_stream((EinaArtwork *) object,  g_value_get_object(value));
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+	}
+}
+
+static void
+eina_artwork_dispose (GObject *object)
+{
+	G_OBJECT_CLASS (eina_artwork_parent_class)->dispose (object);
+}
+
+static void
+eina_artwork_finalize (GObject *object)
+{
+	G_OBJECT_CLASS (eina_artwork_parent_class)->finalize (object);
+}
+
+static void
+eina_artwork_class_init (EinaArtworkClass *klass)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+	g_type_class_add_private (klass, sizeof (EinaArtworkPrivate));
+
+	object_class->get_property = eina_artwork_get_property;
+	object_class->set_property = eina_artwork_set_property;
+	object_class->dispose = eina_artwork_dispose;
+	object_class->finalize = eina_artwork_finalize;
+}
+
+static void
+eina_artwork_init (EinaArtwork *self)
+{
+}
+
+EinaArtwork*
+eina_artwork_new (void)
+{
+	return g_object_new (EINA_TYPE_ARTWORK, NULL);
+}
+
+
+void
+eina_artwork_set_stream(EinaArtwork *self, LomoStream *stream)
+{
+	EinaArtworkPrivate *priv = GET_PRIVATE(self);
+
+	gel_warn("_set_stream called");
+
+	// Stop any running providers
+	if (priv->running)
+	{
+		gel_warn("Provider running. Cancel it");
+		eina_artwork_provider_cancel((EinaArtworkProvider *) priv->running->data, self);
+		priv->running = NULL;
+	}
+
+	priv->stream = stream;
+
+	// Start providers
+	gel_warn("Start new queue of providers");
+	priv->running = priv->providers;
+	run_queue(self);
+}
+
+LomoStream *
+eina_artwork_get_stream(EinaArtwork *self)
+{
+	return GET_PRIVATE(self)->stream;
+}
+
+gboolean
+eina_artwork_add_provider(EinaArtwork *self,
+	const gchar *name,
+	EinaArtworkProviderSearchFunc search, EinaArtworkProviderCancelFunc cancel,
+	gpointer provider_data)
+{
+	EinaArtworkPrivate *priv = GET_PRIVATE(self);
+
+	// Search plugin
+	if (search_provider(self, name))
+		return FALSE;
+
+	// Add provider al end of queue
+	priv->providers = g_list_append(priv->providers, eina_artwork_provider_new(name, search, cancel, provider_data));
+
+	return TRUE;
+}
+void
+eina_artwork_provider_success(EinaArtwork *self, GType type, gpointer data)
+{
+	if (type == G_TYPE_STRING)
+		gtk_image_set_from_file(GTK_IMAGE(self), (gchar *) data);
+
+	else if (type == GDK_TYPE_PIXBUF)
+		gtk_image_set_from_pixbuf(GTK_IMAGE(self), GDK_PIXBUF(data));
+
+	else
+	{
+		gel_warn("Unknow result type");
+		return;
+	}
+
+	// Set queue to idle
+	GET_PRIVATE(self)->running = NULL;
+}
+
+void
+eina_artwork_provider_fail(EinaArtwork *self)
+{
+	EinaArtworkPrivate *priv = GET_PRIVATE(self);
+
+	// Move to next provider
+	priv->running = priv->running->next;
+
+	// Run queue
+	run_queue(self);
+}
+
+static gboolean
+search_provider(EinaArtwork *self, const gchar *name)
+{
+	GList *iter = GET_PRIVATE(self)->providers;
+	while (iter)
+	{
+		if (g_str_equal(((EinaArtworkProvider *) iter->data)->name, name))
+			return TRUE;
+		iter = iter->next;
+	}
+	return FALSE;
+}
+
+static void
+run_queue(EinaArtwork *self)
+{
+	EinaArtworkPrivate *priv = GET_PRIVATE(self);
+	if (priv->stream == NULL)
+	{
+		gel_warn("No stream. Stop");
+		priv->running = NULL;
+		return;
+	}
+	if (priv->running == NULL)
+	{
+		gel_warn("No provider. Stop");
+		return;
+	}
+
+	eina_artwork_provider_search((EinaArtworkProvider*) priv->running->data, self, priv->stream);
+}
+
+EinaArtworkProvider *
+eina_artwork_provider_new(const gchar *name, EinaArtworkProviderSearchFunc search, EinaArtworkProviderCancelFunc cancel, gpointer provider_data)
+{
+	EinaArtworkProvider *self = g_new0(EinaArtworkProvider, 1);
+	self->name   = name;
+	self->search = search;
+	self->cancel = cancel;
+	self->data   = provider_data;
+	return self;
+}
+
+void
+eina_artwork_provider_free(EinaArtworkProvider *self)
+{
+	g_free(self);
+}
+
+void
+eina_artwork_provider_search(EinaArtworkProvider *self, EinaArtwork *artwork, LomoStream *stream)
+{
+	if (self->search)
+		self->search(artwork, stream, self->data);
+}
+
+void
+eina_artwork_provider_cancel(EinaArtworkProvider *self, EinaArtwork *artwork)
+{
+	if (self->cancel)
+		self->cancel(artwork, self->data);
+}
+
+
---
+++ eina/eina-artwork.h
@@ -0,0 +1,75 @@
+#ifndef _EINA_ARTWORK
+#define _EINA_ARTWORK
+
+#include <gtk/gtk.h>
+#include <lomo/stream.h>
+
+G_BEGIN_DECLS
+
+#define EINA_TYPE_ARTWORK eina_artwork_get_type()
+
+#define EINA_ARTWORK(obj) \
+	(G_TYPE_CHECK_INSTANCE_CAST ((obj), EINA_TYPE_ARTWORK, EinaArtwork))
+
+#define EINA_ARTWORK_CLASS(klass) \
+	(G_TYPE_CHECK_CLASS_CAST ((klass), EINA_TYPE_ARTWORK, EinaArtworkClass))
+
+#define EINA_IS_ARTWORK(obj) \
+	(G_TYPE_CHECK_INSTANCE_TYPE ((obj), EINA_TYPE_ARTWORK))
+
+#define EINA_IS_ARTWORK_CLASS(klass) \
+	(G_TYPE_CHECK_CLASS_TYPE ((klass), EINA_TYPE_ARTWORK))
+
+#define EINA_ARTWORK_GET_CLASS(obj) \
+	(G_TYPE_INSTANCE_GET_CLASS ((obj), EINA_TYPE_ARTWORK, EinaArtworkClass))
+
+typedef struct {
+	GtkImage parent;
+} EinaArtwork;
+
+typedef struct {
+	GtkImageClass parent_class;
+	void (*change) (EinaArtwork *self);
+} EinaArtworkClass;
+
+typedef void (*EinaArtworkProviderSearchFunc) (EinaArtwork *self, LomoStream *stream, gpointer data);
+typedef void (*EinaArtworkProviderCancelFunc) (EinaArtwork *self, gpointer data);
+
+GType eina_artwork_get_type (void);
+
+EinaArtwork* eina_artwork_new (void);
+
+void
+eina_artwork_set_stream(EinaArtwork *self, LomoStream *stream);
+LomoStream *
+eina_artwork_get_stream(EinaArtwork *self);
+
+gboolean
+eina_artwork_add_provider(EinaArtwork *self,
+	const gchar *name,
+	EinaArtworkProviderSearchFunc search, EinaArtworkProviderCancelFunc cancel,
+	gpointer provider_data);
+
+void
+eina_artwork_provider_success(EinaArtwork *self, GType type, gpointer data);
+void
+eina_artwork_provider_fail(EinaArtwork *self);
+
+#ifdef EINA_COMPILATION
+typedef struct _EinaArtworkProvider EinaArtworkProvider;
+
+EinaArtworkProvider *
+eina_artwork_provider_new(const gchar *name, EinaArtworkProviderSearchFunc search, EinaArtworkProviderCancelFunc cancel, gpointer provider_data);
+void
+eina_artwork_provider_free(EinaArtworkProvider *self);
+
+void
+eina_artwork_provider_search(EinaArtworkProvider *self, EinaArtwork *artwork, LomoStream *stream);
+void
+eina_artwork_provider_cancel(EinaArtworkProvider *self, EinaArtwork *artwork);
+
+#endif
+
+G_END_DECLS
+
+#endif /* _EINA_ARTWORK */
