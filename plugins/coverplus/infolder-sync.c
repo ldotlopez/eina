73aec45aa7280f52a0b40e9d71297bd71aab6285
Luis Lopez <xuzo@cuarentaydos.com>
2008-12-11 20:16:37 +0000

    Add missing infolder-sync.c

---
+++ plugins/coverplus/infolder-sync.c
@@ -0,0 +1,124 @@
+#define GEL_DOMAIN "Eina::Plugin::CoverPlus::Infolder-Sync"
+
+#include <config.h>
+#include <glib/gstdio.h>
+#include <gdk/gdk.h>
+#include <gel/gel-io.h>
+#include <eina/plugin.h>
+#include "infolder.h"
+
+static gchar *coverplus_infolder_regex_str[] = {
+	".*front.*\\.(jpe?g|png|gif)$",
+	".*cover.*\\.(jpe?g|png|gif)$",
+	".*folder.*\\.(jpe?g|png|gif)$",
+	".*\\.(jpe?g|png|gif)$",
+	NULL
+};
+
+struct _CoverPlusInfolder {
+	EinaArtwork  *cover;
+	GRegex       *regexes[4]; // Keep in sync with size of coverplus_infolder_regex_str
+	GelIOOp      *async_op;
+	GCancellable *cancellable;
+	gint          score;
+};
+
+CoverPlusInfolder *
+coverplus_infolder_new(EinaPlugin *plugin, GError **error)
+{
+	CoverPlusInfolder *self = g_new0(CoverPlusInfolder, 1);
+
+	// Compile regexes
+	gint i;
+	for (i = 0; coverplus_infolder_regex_str[i] != NULL; i++)
+	{
+		GError *err = NULL;
+		self->regexes[i] = g_regex_new(coverplus_infolder_regex_str[i],
+			G_REGEX_CASELESS|G_REGEX_DOTALL|G_REGEX_DOLLAR_ENDONLY|G_REGEX_OPTIMIZE|G_REGEX_NO_AUTO_CAPTURE,
+			0, &err);
+		if (!self->regexes[i])
+		{
+			gel_error("Cannot compile regex '%s': '%s'", coverplus_infolder_regex_str[i], err->message);
+			g_error_free(err);
+			coverplus_infolder_destroy(self);
+			return NULL;
+		}
+	}
+
+	return self;
+}
+
+void
+coverplus_infolder_destroy(CoverPlusInfolder *self)
+{
+	// Free regexes
+	gint i;
+	for (i = 0; coverplus_infolder_regex_str[i] != NULL; i++)
+		if (self->regexes[i])
+			g_regex_unref(self->regexes[i]);
+
+	// Bye
+	g_free(self);
+}
+
+void
+coverplus_infolder_search_cb(EinaArtwork *artwork, LomoStream *stream, CoverPlusInfolder *self)
+{
+	gchar *uri = lomo_stream_get_tag(stream, LOMO_TAG_URI);
+	gchar *scheme =  g_uri_parse_scheme(uri);
+	if (!g_str_equal(scheme, "file"))
+	{
+		gel_warn("sync search using coverplus-infolder only works in local files");
+		g_free(scheme);
+		eina_artwork_provider_fail(artwork);
+		return;
+	}
+	g_free(scheme);
+
+	GError *error = NULL;
+	gchar *dirname = g_path_get_dirname(uri);
+	gchar *d =g_filename_from_uri(dirname, NULL, NULL);
+	GList *children = gel_dir_read(d, FALSE, &error);
+	if (error)
+	{
+		gel_warn("Error reading %s: %s", dirname, error->message);
+		g_free(dirname);
+		g_error_free(error);
+		eina_artwork_provider_fail(artwork);
+		return;
+	}
+
+	GList *iter = children;
+	gchar *winner = NULL;
+	gint score = G_MAXINT;
+	while (iter)
+	{
+		gint i;
+		for (i = 0; coverplus_infolder_regex_str[i] != NULL; i++)
+		{
+			if (self->regexes[i] && g_regex_match(self->regexes[i], (gchar *) iter->data, 0, NULL) && (score > i))
+			{
+				winner = iter->data;
+				score = i;
+			}
+		}
+		iter = iter->next;
+	}
+
+	if (score < G_MAXINT)
+	{	
+		gel_warn("Got winner: %s", winner);
+	}
+	gel_list_deep_free(children, g_free);
+
+	g_free(dirname);
+
+	eina_artwork_provider_fail(artwork);
+}
+
+void
+coverplus_infolder_cancel_cb(EinaArtwork *artwork, CoverPlusInfolder *self)
+{
+	
+}
+
