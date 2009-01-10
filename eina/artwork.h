40f814ae5ce885d70cfe45f88034352ef4fcea25
Luis Lopez <xuzo@cuarentaydos.com>
2009-01-10 20:21:13 +0000

    Fix wrong define which still refernces to EinaBase

--- eina/artwork.h
+++ eina/artwork.h
@@ -5,6 +5,6 @@
 #include <eina/eina-artwork.h>
 
 #define GEL_HUB_GET_ARTWORK(hub)    EINA_ARTWORK(gel_hub_shared_get(hub, "artwork"))
-#define EINA_BASE_GET_ARTWORK(base) GEL_HUB_GET_ARTWORK(EINA_BASE(base)->hub)
+#define EINA_OBJ_GET_ARTWORK(base) GEL_HUB_GET_ARTWORK(EINA_BASE(base)->hub)
 
 #endif
