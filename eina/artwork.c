1c22e885c3967d3cba7b4443041d0c45ce46ac05
Luis Lopez <xuzo@cuarentaydos.com>
2009-01-13 11:46:47 +0000

    Move artwork plugin

--- eina/artwork.c
+++ eina/artwork.c
@@ -1,18 +1,35 @@
 #define GEL_DOMAIN "Eina::ArtworkSingleton"
 
-#include <eina/base.h>
-#include <eina/lomo.h>
+#include <config.h>
+#include <gel/gel.h>
 #include <eina/artwork.h>
-#include <eina/plugin.h>
 
-G_MODULE_EXPORT gboolean artwork_init
-(GelHub *hub, gint *argc, gchar ***argv)
+enum {
+	ARTWORK_NO_ERROR = 0,
+	ARTWORK_ERROR_CANNOT_REGISTER_OBJECT,
+	ARTWORK_ERROR_CANNOT_UNREGISTER_OBJECT
+};
+
+static GQuark
+artwork_quark(void)
 {
-	EinaArtwork *obj = eina_artwork_new();
+	static GQuark ret = 0;
+	if (ret == 0)
+		ret = g_quark_from_static_string("artwork");
+	return ret;
+}
 
-	if (!gel_hub_shared_set(hub, "artwork", obj))
+static gboolean
+artwork_init (GelPlugin *plugin, GError **error)
+{
+	GelApp *app = gel_plugin_get_app(plugin);
+	EinaArtwork *obj = eina_artwork_new();
+	g_object_ref(obj);
+
+	if (!gel_app_shared_set(app, "artwork", obj))
 	{
-		gel_error("Cannot create singleton for EinaArtwork object");
+		g_set_error(error, artwork_quark(), ARTWORK_ERROR_CANNOT_REGISTER_OBJECT,
+			N_("Cannot create singleton for EinaArtwork object"));
 		g_object_unref(obj);
 		return FALSE;
 	}
@@ -20,19 +37,28 @@ G_MODULE_EXPORT gboolean artwork_init
 	return TRUE;
 }
 
-G_MODULE_EXPORT gboolean artwork_exit
-(gpointer data)
+static gboolean
+artwork_fini(GelPlugin *plugin, GError **error)
 {
-	/*
-	EinaArtwork *obj = EINA_ARTWORK(data);
+	GelApp *app = gel_plugin_get_app(plugin);
+	EinaArtwork *obj = gel_app_shared_get(app, "artwork");
+	if (!obj)
+	{
+		g_set_error(error, artwork_quark(), ARTWORK_ERROR_CANNOT_UNREGISTER_OBJECT,
+			N_("Cannot fetch singleton for EinaArtwork object"));
+		return FALSE;
+	}
+	gel_app_shared_unregister(app, "artwork");
 	g_object_unref(obj);
-	*/
 	return TRUE;
 }
 
-G_MODULE_EXPORT GelHubSlave artwork_connector = {
-	"artwork",
-	&artwork_init,
-	&artwork_exit
+G_MODULE_EXPORT GelPlugin artwork_plugin = {
+	GEL_PLUGIN_SERIAL,
+	"artwork", PACKAGE_VERSION,
+	N_("Build-in artwork plugin"), NULL,
+	NULL, NULL, NULL,
+	artwork_init, artwork_fini,
+	NULL, NULL
 };
 
--- eina/artwork.h
+++ eina/artwork.h
@@ -1,10 +1,10 @@
 #ifndef _ARTWORK_H
 #define _ARTWORK_H
 
-#include <eina/base.h>
+#include <eina/eina-obj.h>
 #include <eina/eina-artwork.h>
 
-#define GEL_HUB_GET_ARTWORK(hub)    EINA_ARTWORK(gel_hub_shared_get(hub, "artwork"))
-#define EINA_BASE_GET_ARTWORK(base) GEL_HUB_GET_ARTWORK(EINA_BASE(base)->hub)
+#define GEL_APP_GET_ARTWORK(app)    EINA_ARTWORK(gel_app_shared_get(app, "artwork"))
+#define EINA_OBJ_GET_ARTWORK(obj)   GEL_APP_GET_ARTWORK(eina_obj_get_app(obj))
 
 #endif
