be0040e61e4fadfcd606d18effdf09fa99dc865a
Luis Lopez <xuzo@cuarentaydos.com>
2009-01-14 08:18:57 +0000

    Remove GelHub

--- gel/gel.h
+++ gel/gel.h
@@ -2,7 +2,7 @@
 #define _GEL_H
 
 #include <gel/gel-app.h>
-#include <gel/gel-hub.h>
+#include <gel/gel-plugin.h>
 #include <gel/gel-string.h>
 #include <gel/gel-str-parser.h>
 #include <gel/gel-misc.h>
