cf50ac328d43aa49b8b97b8b64bb3d0d482f8a1d
Luis Lopez <xuzo@cuarentaydos.com>
2009-04-03 13:15:26 +0000

    Bundle for OSX (I)

---
+++ bundle/environment.sh
@@ -0,0 +1,21 @@
+#Gst env
+export GST_PLUGIN_PATH=$bundle_res/lib/gstreamer-0.10
+export GST_PLUGIN_SYSTEM_PATH=$bundle_res/lib/gstreamer-0.10
+export GST_REGISTRY_UPDATE="no"
+
+#Pango env
+export PANGO_RC_FILE=$bundle_res/etc/pango/pangorc
+cat > $bundle_res/etc/pango/pangorc <<EOF
+[Pango]
+ModuleFiles=$bundle_res/etc/pango/pango.modules
+EOF
+
+cat $bundle_res/etc/pango/pango.modules | sed -e "s,^.*\(/lib/pango/1.6.0/*\),$bundle_res\1,g" > $bundle_res/etc/pango/pango.modules.tmp
+mv $bundle_res/etc/pango/pango.modules.tmp  $bundle_res/etc/pango/pango.modules
+
+#export GST_DEBUG="*:5"
+#export GST_DEBUG_NO_COLOR=1
+
+#Eina env
+export EINA_PIXMAPS_DIR=$bundle_res/share/eina/pixmaps
+export EINA_UI_DIR=$bundle_res/share/eina/ui
