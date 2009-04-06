fd7a97f39694542236f286bedb72475746b73df1
Luis Lopez <xuzo@cuarentaydos.com>
2009-04-06 12:16:33 +0000

    Added fixes to Eina.app

--- bundle/environment.sh
+++ bundle/environment.sh
@@ -11,7 +11,9 @@ ModuleFiles=$bundle_res/etc/pango/pango.modules
 EOF
 
 cat $bundle_res/etc/pango/pango.modules | sed -e "s,^.*\(/lib/pango/1.6.0/*\),$bundle_res\1,g" > $bundle_res/etc/pango/pango.modules.tmp
-mv $bundle_res/etc/pango/pango.modules.tmp  $bundle_res/etc/pango/pango.modules
+if [ $? -eq 0 ]; then
+	mv $bundle_res/etc/pango/pango.modules.tmp  $bundle_res/etc/pango/pango.modules
+fi
 
 #export GST_DEBUG="*:5"
 #export GST_DEBUG_NO_COLOR=1
@@ -19,3 +21,4 @@ mv $bundle_res/etc/pango/pango.modules.tmp  $bundle_res/etc/pango/pango.modules
 #Eina env
 export EINA_PIXMAPS_DIR=$bundle_res/share/eina/pixmaps
 export EINA_UI_DIR=$bundle_res/share/eina/ui
+export EINA_PLUGINS_PATH="$bundle_res/lib/eina"
