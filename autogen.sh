3a4b47ee9c200367bc436368a42747b9c09251de
Luis Lopez <xuzo@cuarentaydos.com>
2009-05-13 11:43:30 +0000

    Some updates on auto* shit

--- autogen.sh
+++ autogen.sh
@@ -1,159 +1,158 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
+CONFIGURE=configure.in
+
+: ${AUTOCONF=autoconf}
+: ${AUTOHEADER=autoheader}
+: ${AUTOMAKE=automake}
+: ${ACLOCAL=aclocal}
+: ${INTLTOOLIZE=intltoolize}
+: ${LIBTOOLIZE=libtoolize}
+#: ${GTKDOCIZE=gtkdocize}
+
+#
+# Nothing should need changing below.
+#
+
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
+ORIGDIR=`pwd`
+cd $srcdir
+
 DIE=0
 
-if [ -n "$GNOME2_DIR" ]; then
-	ACLOCAL_FLAGS="-I $GNOME2_DIR/share/aclocal $ACLOCAL_FLAGS"
-	LD_LIBRARY_PATH="$GNOME2_DIR/lib:$LD_LIBRARY_PATH"
-	PATH="$GNOME2_DIR/bin:$PATH"
-	export PATH
-	export LD_LIBRARY_PATH
-fi
-
-(test -f $srcdir/configure.in) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level package directory"
-    exit 1
-}
-
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+($AUTOCONF --version) < /dev/null > /dev/null 2>&1 || {
   echo
-  echo "**Error**: You must have \`autoconf' installed."
+  echo "You must have autoconf installed to compile this project."
   echo "Download the appropriate package for your distribution,"
   echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
   DIE=1
 }
 
-(grep "^AC_PROG_INTLTOOL" $srcdir/configure.in >/dev/null) && {
-  (intltoolize --version) < /dev/null > /dev/null 2>&1 || {
-    echo 
-    echo "**Error**: You must have \`intltool' installed."
-    echo "You can get it from:"
-    echo "  ftp://ftp.gnome.org/pub/GNOME/"
-    DIE=1
-  }
-}
-
-(grep "^AM_PROG_XML_I18N_TOOLS" $srcdir/configure.in >/dev/null) && {
-  (xml-i18n-toolize --version) < /dev/null > /dev/null 2>&1 || {
+(grep "^IT_PROG_INTLTOOL" $srcdir/$CONFIGURE >/dev/null) && {
+  ($INTLTOOLIZE --version) < /dev/null > /dev/null 2>&1 || {
     echo
-    echo "**Error**: You must have \`xml-i18n-toolize' installed."
-    echo "You can get it from:"
-    echo "  ftp://ftp.gnome.org/pub/GNOME/"
+    echo "You must have intltoolize installed to compile this project."
+    echo "Get ftp://ftp.gnome.org/pub/GNOME/stable/sources/intltool/intltool-0.35.tar.gz"
+    echo "(or a newer version if it is available)"
     DIE=1
   }
 }
 
-(grep "^AM_PROG_LIBTOOL" $srcdir/configure.in >/dev/null) && {
-  (libtool --version) < /dev/null > /dev/null 2>&1 || {
-    echo
-    echo "**Error**: You must have \`libtool' installed."
-    echo "You can get it from: ftp://ftp.gnu.org/pub/gnu/"
-    DIE=1
-  }
-}
+# Check if gtk-doc is explicitly disabled.
+#for option in $AUTOGEN_CONFIGURE_ARGS $@
+#do
+#  case $option in
+#    -disable-gtk-doc | --disable-gtk-doc)
+#    enable_gtk_doc=no
+#  ;;
+#  esac
+#done
+#
+#if test x$enable_gtk_doc != xno; then
+#  echo "Checking for gtkdocize ... "
+#  if grep "^GTK_DOC_CHECK" $CONFIGURE > /dev/null; then
+#    if !($GTKDOCIZE --version) < /dev/null > /dev/null 2>&1; then
+#      echo
+#      echo "  You must have gtk-doc installed to compile this project."
+#      echo "  Install the appropriate package for your distribution,"
+#      echo "  or get the source tarball at"
+#      echo "  http://ftp.gnome.org/pub/GNOME/sources/gtk-doc/"
+#      echo "  You can also use the option --disable-gtk-doc to skip"
+#      echo "  this test but then you will not be able to generate a"
+#      echo "  configure script that can build the API documentation."
+#      DIE=1
+#    fi
+#  fi
+#fi
 
-(grep "^AM_GLIB_GNU_GETTEXT" $srcdir/configure.in >/dev/null) && {
-  (grep "sed.*POTFILES" $srcdir/configure.in) > /dev/null || \
-  (glib-gettextize --version) < /dev/null > /dev/null 2>&1 || {
-    echo
-    echo "**Error**: You must have \`glib' installed."
-    echo "You can get it from: ftp://ftp.gtk.org/pub/gtk"
-    DIE=1
-  }
-}
-
-(automake --version) < /dev/null > /dev/null 2>&1 || {
+($AUTOMAKE --version) < /dev/null > /dev/null 2>&1 || {
   echo
-  echo "**Error**: You must have \`automake' installed."
-  echo "You can get it from: ftp://ftp.gnu.org/pub/gnu/"
-  DIE=1
-  NO_AUTOMAKE=yes
-}
-
-
-# if no automake, don't bother testing for aclocal
-test -n "$NO_AUTOMAKE" || (aclocal --version) < /dev/null > /dev/null 2>&1 || {
-  echo
-  echo "**Error**: Missing \`aclocal'.  The version of \`automake'"
-  echo "installed doesn't appear recent enough."
-  echo "You can get automake from ftp://ftp.gnu.org/pub/gnu/"
+  echo "You must have automake installed to compile this project."
+  echo "Get ftp://sourceware.cygnus.com/pub/automake/automake-1.9.tar.gz"
+  echo "(or a newer version if it is available)"
   DIE=1
 }
+
+($LIBTOOLIZE --version) < /dev/null > /dev/null 2>&1 || {
+  echo
+  echo "You must have libtool installed to compile this project."
+  echo "Get ftp://ftp.gnu.org/pub/gnu/libtool-1.5.22.tar.gz"
+  echo "(or a newer version if it is available)"
+  DIE=1
+}
+
+if grep "^AM_[A-Z0-9_]\{1,\}_GETTEXT" "$CONFIGURE" >/dev/null; then
+  if grep "sed.*POTFILES" "$CONFIGURE" >/dev/null; then
+    GETTEXTIZE=""
+  else
+    if grep "^AM_GLIB_GNU_GETTEXT" "$CONFIGURE" >/dev/null; then
+      GETTEXTIZE="glib-gettextize"
+      GETTEXTIZE_URL="ftp://ftp.gtk.org/pub/gtk/v2.0/glib-2.0.0.tar.gz"
+    else
+      GETTEXTIZE="gettextize"
+      GETTEXTIZE_URL="ftp://alpha.gnu.org/gnu/gettext-0.10.35.tar.gz"
+    fi
+
+    $GETTEXTIZE --version < /dev/null > /dev/null 2>&1
+    if test $? -ne 0; then
+      echo
+      echo "You must have $GETTEXTIZE installed to compile this project."
+      echo "Get $GETTEXTIZE_URL"
+      echo "(or a newer version if it is available)"
+      DIE=1
+    fi
+  fi
+fi
 
 if test "$DIE" -eq 1; then
   exit 1
 fi
 
-if test -z "$*"; then
-  echo "**Warning**: I am going to run \`configure' with no arguments."
-  echo "If you wish to pass any to it, please specify them on the"
-  echo \`$0\'" command line."
-  echo
+test -f $CONFIGURE || {
+  echo "You must run this script in the top-level this project directory"
+  exit 1
+}
+
+rm -rf autom4te.cache
+
+do_cmd() {
+  echo "Running '$@'"
+  $@ || exit $?
+}
+
+do_cmd $ACLOCAL $ACLOCAL_FLAGS
+
+if grep "^IT_PROG_INTLTOOL" $CONFIGURE >/dev/null; then
+  do_cmd $INTLTOOLIZE --copy --force --automake
 fi
 
-case $CC in
-xlc )
-  am_opt=--include-deps;;
-esac
+do_cmd $LIBTOOLIZE --copy --force --automake
 
-for coin in `find $srcdir -path $srcdir/CVS -prune -o -name configure.in -print`
-do 
-  dr=`dirname $coin`
-  if test -f $dr/NO-AUTO-GEN; then
-    echo skipping $dr -- flagged as no auto-gen
+if grep "^GTK_DOC_CHECK" $CONFIGURE > /dev/null; then
+  if test x$enable_gtk_doc = xno; then
+    echo "WARNING: You have disabled gtk-doc."
+    echo "         As a result, you will not be able to generate the API"
+    echo "         documentation and 'make dist' will not work."
+    echo
   else
-    echo processing $dr
-    ( cd $dr
-
-      aclocalinclude="$ACLOCAL_FLAGS"
-
-      if grep "^AM_GLIB_GNU_GETTEXT" configure.in >/dev/null; then
-	echo "Creating $dr/aclocal.m4 ..."
-	test -r $dr/aclocal.m4 || touch $dr/aclocal.m4
-	echo "Running glib-gettextize...  Ignore non-fatal messages."
-	echo "no" | glib-gettextize --force --copy
-	echo "Making $dr/aclocal.m4 writable ..."
-	test -r $dr/aclocal.m4 && chmod u+w $dr/aclocal.m4
-      fi
-      if grep "^AC_PROG_INTLTOOL" configure.in >/dev/null; then
-        echo "Running intltoolize..."
-	intltoolize --copy --force --automake
-      fi
-      if grep "^AM_PROG_XML_I18N_TOOLS" configure.in >/dev/null; then
-        echo "Running xml-i18n-toolize..."
-	xml-i18n-toolize --copy --force --automake
-      fi
-      if grep "^AM_PROG_LIBTOOL" configure.in >/dev/null; then
-	if test -z "$NO_LIBTOOLIZE" ; then 
-	  echo "Running libtoolize..."
-	  libtoolize --force --copy
-	fi
-      fi
-      echo "Running aclocal $aclocalinclude ..."
-      aclocal $aclocalinclude
-      if grep "^AM_CONFIG_HEADER" configure.in >/dev/null; then
-	echo "Running autoheader..."
-	autoheader
-      fi
-      echo "Running automake --gnu $am_opt ..."
-      automake --add-missing --gnu $am_opt
-      echo "Running autoconf ..."
-      autoconf
-    )
+    do_cmd $GTKDOCIZE --copy --flavour no-tmpl --docdir build
   fi
-done
+fi
 
-conf_flags="--enable-maintainer-mode"
+do_cmd $AUTOHEADER
+
+do_cmd $AUTOMAKE --add-missing -Wall
+
+do_cmd $AUTOCONF
+
+cd $ORIGDIR || exit $?
 
 if test x$NOCONFIGURE = x; then
-  echo Running $srcdir/configure $conf_flags "$@" ...
-  $srcdir/configure $conf_flags "$@" \
-  && echo Now type \`make\' to compile. || exit 1
+  echo "Running '$srcdir/configure $@'"
+  $srcdir/configure "$@" && echo "Now type 'make' to compile."  || exit 1
 else
   echo Skipping configure process.
 fi
