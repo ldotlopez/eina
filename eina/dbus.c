db7303f3ac94787c2ac0104b2f4fad7e0bd52065
Luis Lopez <xuzo@cuarentaydos.com>
2009-10-30 20:49:20 +0100

    [DBus]
    Add dbus bindings for LomoPlayer object

--- eina/dbus.c
+++ eina/dbus.c
@@ -19,9 +19,13 @@
 #define GEL_DOMAIN "Eina::DBus"
 #define EINA_PLUGIN_DATA_TYPE DBusGProxy
 #include <config.h>
+#include <eina/dbus.h>
 #include <eina/eina-plugin.h>
+#include <dbus/dbus.h>
 #include <dbus/dbus-glib.h>
+#include <dbus/dbus-glib-bindings.h>
 #include <eina/dbus-marshallers.h>
+#include "../data/lomo-player-glue.h"
 
 /*
  * dbus-send --session --type=method_call --print-reply
@@ -32,6 +36,8 @@
  *   --dest=org.gnome.SettingsDaemon /org/gnome/SettingsDaemon/MediaKeys
  *   org.freedesktop.DBus.Introspectable.Introspect
  */
+
+GEL_AUTO_QUARK_FUNC(eina_dbus)
 
 static void
 key_pressed
@@ -75,11 +81,49 @@ key_pressed
 }
 
 static gboolean
+dbus_register_server(GelApp *app, DBusGConnection *conn, GError **error)
+{
+	DBusGProxy *proxy = dbus_g_proxy_new_for_name(conn,
+		DBUS_SERVICE_DBUS,
+		DBUS_PATH_DBUS,
+		DBUS_INTERFACE_DBUS);
+	if (proxy == NULL)
+	{
+		g_set_error(error, eina_dbus_quark(), EINA_DBUS_CANNOT_GET_DBUS_PROXY,
+			N_("Cannot get DBus proxy"));
+		return FALSE;
+	}
+
+	guint result;
+    org_freedesktop_DBus_request_name (proxy,
+		"net.sourceforge.Eina.LomoPlayer", 
+		DBUS_NAME_FLAG_DO_NOT_QUEUE, &result, error);
+	if (*error)
+	{
+		g_object_unref(proxy);
+		return FALSE;
+	}
+
+	LomoPlayer *lomo = gel_app_get_lomo(app);
+	dbus_g_object_type_install_info(G_OBJECT_TYPE(lomo), &dbus_glib_lomo_player_object_info);
+	dbus_g_connection_register_g_object (conn, "/net/sourceforge/Eina", (GObject*) lomo);
+
+	return TRUE;
+}
+
+static gboolean
 dbus_init(GelApp *app, EinaPlugin *plugin, GError **error)
 {
  	DBusGConnection *connection = dbus_g_bus_get(DBUS_BUS_SESSION, error);
 	if (connection == NULL)
 		return FALSE;
+
+	GError *err = NULL;
+	if (!dbus_register_server(app, connection, &err))
+	{
+		gel_warn(N_("Cannot register DBus server: %s"), err->message);
+		g_error_free(err);
+	}
 
 	DBusGProxy *proxy = dbus_g_proxy_new_for_name_owner(connection,
 		"org.gnome.SettingsDaemon",
---
+++ eina/dbus.h
@@ -0,0 +1,35 @@
+/*
+ * eina/dbus.h
+ *
+ * Copyright (C) 2004-2009 Eina
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _EINA_DBUS_H
+#define _EINA_DBUS_H
+
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+typedef enum { 
+	EINA_DBUS_NO_ERROR = 0, 
+	EINA_DBUS_CANNOT_GET_DBUS_PROXY
+} EinaDBusError; 
+
+G_END_DECLS
+
+#endif
+
