61951063771bc99aca91319d20e7281149d17da5
Luis Lopez <xuzo@cuarentaydos.com>
2010-06-23 09:26:51 +0200

    [Ige] Update to 0.9.x API

--- eina/eina-ige.c
+++ eina/eina-ige.c
@@ -24,11 +24,13 @@
 #include <eina/eina-plugin.h>
 #include <eina/window.h>
 #include <igemacintegration/ige-mac-integration.h>
+#include <igemacintegration/gtkosxapplication.h>
 
 typedef struct {
-	EinaObj          parent;
+	EinaObj            parent;
+	GtkOSXApplication *osxapp;
 	// IgeMacDock      *dock;
-	IgeMacMenuGroup *about_gr, *prefs_gr;
+	GtkOSXApplicationMenuGroup *about_gr, *prefs_gr;
 	guint flags;
 } EinaIge;
 
@@ -45,6 +47,24 @@ ige_plugin_init(GelApp *app, EinaPlugin *plugin, GError **error)
 	if (!eina_obj_init((EinaObj *) self, plugin, "ige", EINA_OBJ_NONE, error))
 		return FALSE;
 
+	self->osxapp = g_object_new(GTK_TYPE_OSX_APPLICATION, NULL);
+	gtk_osxapplication_set_use_quartz_accelerators(self->osxapp, TRUE);
+
+	GtkUIManager *ui_mng        = eina_window_get_ui_manager(GEL_APP_GET_WINDOW(app));
+	GtkWidget    *main_menu_bar = gtk_ui_manager_get_widget(ui_mng, "/Main");
+	gtk_widget_hide(main_menu_bar);
+
+	gtk_osxapplication_set_menu_bar(self->osxapp, GTK_MENU_SHELL(main_menu_bar));
+	self->about_gr = gtk_osxapplication_add_app_menu_group(self->osxapp);
+	self->prefs_gr = gtk_osxapplication_add_app_menu_group(self->osxapp);
+
+	gtk_osxapplication_ready(self->osxapp);
+	ige_sync_menu(self, NULL);
+
+	g_signal_connect_swapped((GObject *) ui_mng, "actions-changed", (GCallback) ige_sync_menu, self);
+
+	gel_plugin_set_data(plugin, self);
+#if 0
 	// Build dock item (in 10.5+ Cocoa apps automaticaly get icon from app
 	// bundle, in addition 10.6+ deprecates Carbon access to dock, so ige dock
 	// functions are not working anymore)
@@ -87,16 +107,18 @@ ige_plugin_init(GelApp *app, EinaPlugin *plugin, GError **error)
 	ige_sync_menu(self, NULL);
 
 	gel_plugin_set_data(plugin, self);
-
+#endif
 	return TRUE;
 }
 
 G_MODULE_EXPORT gboolean
 ige_plugin_fini(GelApp *app, EinaPlugin *plugin, GError **error)
 {
+#if 0
 	EinaIge *self = EINA_PLUGIN_DATA(plugin);
 	// g_object_unref(self->dock);
 	eina_obj_fini((EinaObj *) self);
+#endif
 	return TRUE;
 }
 
@@ -122,20 +144,24 @@ ige_sync_menu(EinaIge *self, GtkUIManager *ui_mng)
 
 		if (g_str_equal(paths[i], "/Main/File/Quit"))
 		{
+			gtk_widget_hide(widget);
 			ige_mac_menu_set_quit_menu_item((GtkMenuItem *) widget);
 		}
 
 		else if (g_str_equal(paths[i], "/Main/Help/About") && !(self->flags & ABOUT_MENU))
 		{
 			self->flags |= ABOUT_MENU;
-			ige_mac_menu_add_app_menu_item(self->about_gr, (GtkMenuItem *) widget, NULL);
+			gtk_widget_hide(widget);
+			gtk_osxapplication_add_app_menu_item(self->osxapp, self->about_gr, (GtkMenuItem *) widget);
 		}
 		else if (g_str_equal(paths[i], "/Main/Edit/Preferences") && !(self->flags & PREFS_MENU))
 		{
 			self->flags |= PREFS_MENU;
-			ige_mac_menu_add_app_menu_item(self->prefs_gr, (GtkMenuItem *) widget, NULL);
+			gtk_widget_hide(widget);
+			gtk_osxapplication_add_app_menu_item(self->osxapp, self->prefs_gr, (GtkMenuItem *) widget);
 		}
 	}
+	gtk_osxapplication_sync_menubar(self->osxapp);
 }
 
 EINA_PLUGIN_INFO_SPEC(ige,
