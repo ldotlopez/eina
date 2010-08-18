59ac42dc70414fd33b1d7c851221db0ec4e4aae8
Luis Lopez <luis.lopez@cuarentaydos.com>
2010-08-18 16:00:32 +0200

    [Player] Generate header file for __cover_mask_png

--- plugins/player/eina-cover-image.c
+++ plugins/player/eina-cover-image.c
@@ -176,7 +176,7 @@ eina_cover_image_init (EinaCoverImage *self)
 	if (priv->mask == NULL)
 	{
 		GError *err = NULL;
-		if ((priv->mask = gdk_pixbuf_new_from_inline(-1, __cover_mask, FALSE, &err)) == NULL)
+		if ((priv->mask = gdk_pixbuf_new_from_inline(-1, __cover_mask_png, FALSE, &err)) == NULL)
 		{
 			g_warning(N_("Unable to load cover mask: %s"), err->message);
 			priv->m_w = priv->m_h = -1;
