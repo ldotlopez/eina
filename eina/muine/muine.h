f0bde04a9f55edaeffefde59bc308bd5fc657b56
Luis Lopez <luis.lopez@cuarentaydos.com>
2010-08-24 12:44:16 +0200

    [schemas] relocate preferences

--- eina/muine/muine.h
+++ eina/muine/muine.h
@@ -4,7 +4,7 @@
 #include <eina/eina-plugin2.h>
 #include <eina/muine/eina-muine.h>
 
-#define EINA_MUINE_PREFERENCES_DOMAIN EINA_DOMAIN".preferences.plugins.muine"
+#define EINA_MUINE_PREFERENCES_DOMAIN EINA_DOMAIN".preferences.muine"
 #define EINA_MUINE_GROUP_BY_KEY "group-by"
 
 #endif
