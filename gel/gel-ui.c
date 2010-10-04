479a36baacd8ecbadff89bfab90a5ca7bf104b54
Luis Lopez <luis.lopez@cuarentaydos.com>
2010-10-04 18:09:23 +0200

    [Misc]
    * Fix some makefile.am's
    * Use gtk_buildable_get_name instead of gtk_widget_get_name

--- gel/gel-ui.c
+++ gel/gel-ui.c
@@ -153,9 +153,9 @@ gel_ui_container_find_widget(GtkContainer *container, gchar *name)
 	while (iter && (ret == NULL))
 	{
 		GtkWidget *child = (GtkWidget *) iter->data;
-		const gchar *c_name = gtk_widget_get_name(child);
-
-		if (g_str_equal(c_name, name))
+		const gchar *c_name = gtk_buildable_get_name(GTK_BUILDABLE(child));
+		gel_warn("'%s' vs '%s'", c_name, name);
+		if (c_name && g_str_equal(c_name, name))
 			ret = child;
 
 		else if (GTK_IS_CONTAINER(child))
