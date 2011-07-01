463c40f9c0052a22ebbc94bff9ff72db653152ea
Luis Lopez <luis@cuarentaydos.com>
2011-07-02 00:42:10 +0200

    Save current playlist almost instantly

--- eina/lomo/eina-lomo-plugin.c
+++ eina/lomo/eina-lomo-plugin.c
@@ -22,6 +22,11 @@
 GEL_DEFINE_QUARK_FUNC(eina_lomo_plugin)
 EINA_DEFINE_EXTENSION(EinaLomoPlugin, eina_lomo_plugin, EINA_TYPE_LOMO_PLUGIN)
 
+static void     schedule_save_playlist(LomoPlayer *lomo);
+static gboolean save_playlist         (LomoPlayer *lomo);
+
+static guint timeout_id = 0;
+
 static gboolean
 eina_lomo_plugin_activate (EinaActivatable *activatable, EinaApplication *application, GError **error)
 {
@@ -60,6 +65,9 @@ eina_lomo_plugin_activate (EinaActivatable *activatable, EinaApplication *applic
 	for (gint i = 0; i < G_N_ELEMENTS(props); i++)
 		g_settings_bind(settings, props[i], lomo, props[i], G_SETTINGS_BIND_DEFAULT);
 
+	g_signal_connect(lomo, "insert", (GCallback) schedule_save_playlist, NULL);
+	g_signal_connect(lomo, "clear",  (GCallback) save_playlist, NULL);
+
 	return TRUE;
 }
 
@@ -74,31 +82,7 @@ eina_lomo_plugin_deactivate (EinaActivatable *activatable, EinaApplication *appl
 		return FALSE;
 	}
 
-	GString *gs = g_string_new(NULL);
-	GList *i = (GList *) lomo_player_get_playlist(lomo);
-	while (i)
-	{
-		gs = g_string_append(gs, lomo_stream_get_tag(LOMO_STREAM(i->data), LOMO_TAG_URI));
-		gs = g_string_append_c(gs, '\n');
-		i = i->next;
-	}
-	gchar *output = g_build_filename(g_get_user_config_dir(), PACKAGE, "playlist", NULL);
-	gchar *bname = g_path_get_dirname(output);
-
-	if (!g_file_test(bname, G_FILE_TEST_IS_DIR))
-	{
-		if (g_mkdir_with_parents(bname, 0755) == -1)
-		{
-			g_warning(N_("Can't create dir '%s': %s"), bname, strerror(errno));
-			gel_free_and_invalidate(bname,  NULL, g_free);
-			gel_free_and_invalidate(output, NULL, g_free);
-		}
-	}
-
-	GError *err = NULL;
-	if (!output || !g_file_set_contents(output, gs->str, -1, &err))
-		g_warning(N_("Unble to save playlist: %s"), err ? err->message : N_("Missing parent directory"));
-	g_string_free(gs, TRUE);
+	save_playlist(lomo);
 
 	g_object_unref(G_OBJECT(lomo));
 
@@ -118,4 +102,66 @@ eina_application_get_lomo(EinaApplication *application)
 {
 	return LOMO_PLAYER(eina_application_get_interface(application, "lomo"));
 }
+
+/**
+ * schedule_save_playlist:
+ * @lomo: A #LomoPlayer
+ *
+ * Schedules save_playlist after one second
+ */
+static void
+schedule_save_playlist(LomoPlayer *lomo)
+{
+	if (timeout_id)
+		g_source_remove(timeout_id);
+	timeout_id = g_timeout_add_seconds(1, (GSourceFunc) save_playlist, lomo);
+}
+
+/**
+ * save_playlist:
+ * @lomo: A #LomoPlayer
+ *
+ * Saves playlist in ~/XDG_CONFIG_DIR/PACKAGE/playlist
+ *
+ * Returns: %FALSE
+ */
+static gboolean
+save_playlist(LomoPlayer *lomo)
+{
+	timeout_id = 0;
+
+	g_return_val_if_fail(LOMO_IS_PLAYER(lomo), FALSE);
+
+	GString *gs = g_string_new(NULL);
+	GList *i = (GList *) lomo_player_get_playlist(lomo);
+	while (i)
+	{
+		gs = g_string_append(gs, lomo_stream_get_tag(LOMO_STREAM(i->data), LOMO_TAG_URI));
+		gs = g_string_append_c(gs, '\n');
+		i = i->next;
+	}
+	gchar *output = g_build_filename(g_get_user_config_dir(), PACKAGE, "playlist", NULL);
+	gchar *bname = g_path_get_dirname(output);
+
+	if (!g_file_test(bname, G_FILE_TEST_IS_DIR))
+	{
+		if (g_mkdir_with_parents(bname, 0755) == -1)
+		{
+			g_warning(N_("Can't create dir '%s': %s"), bname, strerror(errno));
+			gel_free_and_invalidate(bname,  NULL, g_free);
+			gel_free_and_invalidate(output, NULL, g_free);
+			g_string_free(gs, TRUE);
+			return FALSE;
+		}
+	}
+
+	GError *err = NULL;
+	if (!output || !g_file_set_contents(output, gs->str, -1, &err))
+		g_warning(N_("Unble to save playlist: %s"), err ? err->message : N_("Missing parent directory"));
+
+	g_string_free(gs, TRUE);
+
+	return FALSE;
+}
+
 
