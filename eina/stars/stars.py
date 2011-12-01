d1ef37dad91743e1f1184c86698d7eae41b562cf
Luis López <luis@cuarentaydos.com>
2011-12-01 15:29:29 +0100

    More work on stars plugin
    * Depends on lomo, adb and player
    * Saves data to adb
    * Can read it but can't use it

--- eina/stars/stars.py
+++ eina/stars/stars.py
@@ -12,15 +12,44 @@ class StarsPlugin(GObject.Object, Eina.Activatable):
 	def __init__(self):
 		GObject.Object.__init__ (self)
 
+	def schema_version_0(self):
+		r = self.adb.query_block_exec(
+			(
+				"""CREATE TABLE stars (
+					sid INTEGER NOT NULL,
+					rating INTEGER NOT NULL DEFAULT 0,
+					CONSTRAINT stars_pk PRIMARY KEY(sid),
+					CONSTRAINT stars_fk_sid FOREIGN KEY(sid) REFERENCES streams(sid) ON DELETE CASCADE ON UPDATE CASCADE
+				); """,
+			)
+		)
+		return r
+
 	def do_activate(self, app):
+		self.adb = app.get_adb()
+		self.lomo = app.get_lomo()
+		self.lomo.connect('change', self.lomo_change_cb)
+
+		curr_schema_version = self.adb.schema_get_version('stars')
+		print "Initial schema version: %d" % curr_schema_version
+		updates = (self.schema_version_0,)
+		for i in xrange(curr_schema_version + 1, len(updates)):
+			r = updates[i]()
+			if r:
+				self.adb.schema_set_version('stars', i)
+			else:
+				print "Update %d fail" % i
+
 		box = app.get_player().get_plugins_area()
 
 		p = os.path.dirname(__file__)
 		stars = EinaStars(
 			on_image = os.path.join(p, "star-on.svg"),
 			off_image = os.path.join(p, "star-off.svg"))
+		stars.set_vexpand(True)
+		stars.set_hexpand(False)
+		stars.connect('notify::n-stars', self.stars_notify_n_stars_cb)
 		stars.show_all()
-	
 		box.pack_start(stars, True, True, 0)
 		return True
 
@@ -28,4 +57,31 @@ class StarsPlugin(GObject.Object, Eina.Activatable):
 		box = app.get_player().get_plugins_area()
 		box.remove(self.grid)
 		return True
+
+	def stars_notify_n_stars_cb(self, w, prop_spec):
+		stream = self.lomo.get_nth_stream(self.lomo.get_current())
+		if stream is None:
+			return
+
+		sid = self.adb.lomo_stream_get_sid(stream)
+		n_stars = w.get_n_stars()
+
+		q = "INSERT OR REPLACE INTO stars VALUES(%(sid)d, %(rating)d);" % {'sid' : sid, 'rating' : n_stars}
+		self.adb.query_exec_raw(q)
+
+		print "SID %d got %d stars" % (sid, n_stars)
+		print q
+
+	def lomo_change_cb(self, lomo, f, t):
+		stream = self.lomo.get_nth_stream(t)
+		if stream is None:
+			return
+		sid = self.adb.lomo_stream_get_sid(stream)
+		if sid < 0:
+			return
+
+		print "new sid: ", sid
+
+		q = "SELECT rating FROM stars WHERE sid=%d" % (sid)
+		res = self.adb.query_raw(q)
 
--- eina/stars/widgets.py
+++ eina/stars/widgets.py
@@ -1,98 +1,129 @@
 from gi.repository import Gtk, Gdk, GdkPixbuf, GObject
 
 class EinaStars(Gtk.Grid):
-	__gtype_name__ = 'EinaStars'
+    __gtype_name__ = 'EinaStars'
 
-	def get_n_stars(self):
-		try:
-			n = self.prop_n_stars
-		except AttributeError:
-			return 0
+    #
+    # Handle n-stars 
+    # 
+    def get_n_stars(self):
+        try:
+            n = self.prop_n_stars
+        except AttributeError:
+            return 0
+        return self.prop_n_stars
 
-		return self.prop_n_stars
+    def set_n_stars(self, n):
+        self.prop_n_stars = n
+        self.draw_stars()
 
-	def set_n_stars(self, n):
-		self.prop_n_stars = n
+    #
+    # Properties
+    #
 
-		if not self.get_realized():
-			self.notify("n-stars")
-			return
+    # Number of stars available
+    max_stars = GObject.property(nick='max-stars', type = int,
+        minimum = 1, default = 5,
+        flags = GObject.PARAM_READWRITE | GObject.PARAM_CONSTRUCT_ONLY)
 
-		alloc = self.get_allocation()
-		h = int(alloc.height / 2)
-		w = int((alloc.width / self.max_stars) *0.80)
+    # Active stars
+    n_stars = GObject.property (nick='n-stars', type = int,
+        minimum = 0, default = 0,
+        setter = set_n_stars, getter = get_n_stars)
 
-		if h > w:
-			h = -1
-		else:
-			w = -1
+    # Filename for the "active" star
+    on_image  = GObject.property (nick='on-image',  type = str,
+        flags = GObject.PARAM_READWRITE | GObject.PARAM_CONSTRUCT_ONLY)
 
-		pb_on  = GdkPixbuf.Pixbuf.new_from_file_at_scale(self.on_image,  w , h , True)
-		pb_off = GdkPixbuf.Pixbuf.new_from_file_at_scale(self.off_image, w , h , True)
+    # Filename for the "inactive" star
+    off_image = GObject.property (nick='off-image', type = str,
+        flags = GObject.PARAM_READWRITE | GObject.PARAM_CONSTRUCT_ONLY)
 
-		for i in xrange(0, self.n_stars):
-			self.imgs[i].set_from_pixbuf(pb_on)
+    def __init__ (self, *args, **kwargs):
+        Gtk.Grid.__init__ (self, *args, **kwargs)
+        self.imgs = []
+        for i in xrange(0, self.max_stars):
+            img = Gtk.Image.new()
 
-		for i in xrange(self.n_stars, self.max_stars):
-			self.imgs[i].set_from_pixbuf(pb_off)
+            ev = Gtk.EventBox()
+            ev.add(img)
+            ev.connect('button-press-event', self.click_cb)
 
-		self.notify("n-stars")
+            self.attach (ev, i, 0, 1, 1)
+            self.imgs.append(img)
+            img.show ()
 
-	max_stars = GObject.property(nick='max-stars', type = int,
-		minimum = 1, default = 5,
-		flags = GObject.PARAM_READWRITE | GObject.PARAM_CONSTRUCT_ONLY)
-	n_stars = GObject.property (nick='n-stars', type = int,
-		minimum = 0, default = 0,
-		setter = set_n_stars, getter = get_n_stars)
-	on_image  = GObject.property (nick='on-image',  type = str) #, flags = GObject.PARAM_CONSTRUCT_ONLY)
-	off_image = GObject.property (nick='off-image', type = str) #, flags = GObject.PARAM_CONSTRUCT_ONLY)
+        self.set_column_homogeneous(True)
+        self.set_row_homogeneous(True)
+        self.connect('size-allocate', self.size_allocate_cb)
 
-	def __init__ (self, *args, **kwargs):
-		Gtk.Grid.__init__ (self, *args, **kwargs)
-		self.imgs = []
-		for i in xrange(0, self.max_stars):
-			img = Gtk.Image.new()
+    def do_get_preferred_width(self):
+	#print "do_get_preferred_width (height:%d)" % self.get_allocation().height
+	return (self.max_stars * 8, self.max_stars * 8)
 
-			ev = Gtk.EventBox()
-			ev.add(img)
-			ev.connect('button-press-event', self.click_cb)
+    def do_get_preferred_height(self):
+	#print "do_get_preferred_height (width:%d)" % self.get_allocation().width
+	p = self.get_parent()
+	if p is None:
+		return (8, 8)
+	else:
+		return (p.get_allocation().height, p.get_allocation().height)
 
-			self.attach (ev, i, 0, 1, 1)
-			self.imgs.append(img)
-			img.show ()
+    def go_get_preferred_width_for_height(self, height):
+        return (height * self.max_stars, height * self.max_stars)
 
-		self.set_column_homogeneous(True)
-		self.set_row_homogeneous(True)
-		self.connect('size-allocate', self.size_allocate_cb)
+    def go_get_preferred_height_for_width(self, width):
+        return (width / self.max_stars, width / self.max_stars)
 
-	def size_allocate_cb(self, w, alloc):
-		self.n_stars = self.n_stars
+    def draw_stars(self):
+        if not self.get_realized():
+            return
 
-	def click_cb(self, w, ev):
-		img = w.get_child()
-		for i in xrange(0, self.max_stars):
-			if img == self.imgs[i]:
-				if i == 0 and (self.n_stars == 1):
-					i = -1
-				self.n_stars = i + 1
-				return False
-		return False
+        for i in xrange(0, self.n_stars):
+            self.imgs[i].set_from_pixbuf(self.pb_on)
+
+        for i in xrange(self.n_stars, self.max_stars):
+            self.imgs[i].set_from_pixbuf(self.pb_off)
+
+    def size_allocate_cb(self, w, alloc):
+        h = max(int(alloc.height / 2), 1)
+        w = max(int((alloc.width / self.max_stars) * 0.80), 1)
+
+        if h > w:
+            h = -1
+        else:
+            w = -1
+
+        self.pb_on  = GdkPixbuf.Pixbuf.new_from_file_at_scale(self.on_image,  w , h , True)
+        self.pb_off = GdkPixbuf.Pixbuf.new_from_file_at_scale(self.off_image, w , h , True)
+
+        self.draw_stars()
+
+    def click_cb(self, w, ev):
+        img = w.get_child()
+        for i in xrange(0, self.max_stars):
+            if img == self.imgs[i]:
+                if i == 0 and (self.n_stars == 1):
+                    i = -1
+                self.n_stars = i + 1
+                return False
+        return False
 
 # Testing code
 
 if __name__ == '__main__':
-	def notify_n_stars_cb(w, prop):
-		print "Stars: %d" % w.n_stars
+    def notify_n_stars_cb(w, prop):
+        print "Stars: %d" % w.n_stars
 
-	Gtk.init([])
-	w = Gtk.Window()
-	s = EinaStars(max_stars = 7, n_stars = 2, on_image = 'star-on.svg', off_image = 'star-off.svg')
-	s.connect("notify::n-stars", notify_n_stars_cb)
-	s.set_hexpand(True)
-	s.set_vexpand(True)
+    Gtk.init([])
+    w = Gtk.Window()
+    s = EinaStars(max_stars = 7, n_stars = 2, on_image = 'star-on.svg', off_image = 'star-off.svg')
+    s.connect("notify::n-stars", notify_n_stars_cb)
+    s.set_hexpand(True)
+    s.set_vexpand(True)
 
-	w.add(s)
-	w.show_all()
-	w.connect('delete-event', lambda w,ev : Gtk.main_quit())
-	Gtk.main()
+    w.add(s)
+    w.show_all()
+    w.connect('delete-event', lambda w,ev : Gtk.main_quit())
+    Gtk.main()
 
