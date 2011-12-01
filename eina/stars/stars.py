4d84381a1043496a536a54cb0950a10709d3755b
Luis Lopez <luis@cuarentaydos.com>
2011-12-01 20:30:58 +0100

    First functional version of stars plugin

--- eina/stars/stars.py
+++ eina/stars/stars.py
@@ -31,7 +31,6 @@ class StarsPlugin(GObject.Object, Eina.Activatable):
 		self.lomo.connect('change', self.lomo_change_cb)
 
 		curr_schema_version = self.adb.schema_get_version('stars')
-		print "Initial schema version: %d" % curr_schema_version
 		updates = (self.schema_version_0,)
 		for i in xrange(curr_schema_version + 1, len(updates)):
 			r = updates[i]()
@@ -43,14 +42,14 @@ class StarsPlugin(GObject.Object, Eina.Activatable):
 		box = app.get_player().get_plugins_area()
 
 		p = os.path.dirname(__file__)
-		stars = EinaStars(
+		self.stars = EinaStars(
 			on_image = os.path.join(p, "star-on.svg"),
 			off_image = os.path.join(p, "star-off.svg"))
-		stars.set_vexpand(True)
-		stars.set_hexpand(False)
-		stars.connect('notify::n-stars', self.stars_notify_n_stars_cb)
-		stars.show_all()
-		box.pack_start(stars, True, True, 0)
+		self.stars.set_vexpand(True)
+		self.stars.set_hexpand(False)
+		self.stars.connect('notify::n-stars', self.stars_notify_n_stars_cb)
+		self.stars.show_all()
+		box.pack_start(self.stars, True, True, 0)
 		return True
 
 	def do_deactivate(self, app):
@@ -63,25 +62,25 @@ class StarsPlugin(GObject.Object, Eina.Activatable):
 		if stream is None:
 			return
 
-		sid = self.adb.lomo_stream_get_sid(stream)
-		n_stars = w.get_n_stars()
-
-		q = "INSERT OR REPLACE INTO stars VALUES(%(sid)d, %(rating)d);" % {'sid' : sid, 'rating' : n_stars}
-		self.adb.query_exec_raw(q)
-
-		print "SID %d got %d stars" % (sid, n_stars)
-		print q
+		data = { 'sid' : self.adb.lomo_stream_get_sid(stream), 'rating' : self.stars.n_stars }
+		self.adb.query_exec_raw("INSERT OR REPLACE INTO stars VALUES(%(sid)d, %(rating)d)" % data)
 
 	def lomo_change_cb(self, lomo, f, t):
 		stream = self.lomo.get_nth_stream(t)
 		if stream is None:
 			return
+
 		sid = self.adb.lomo_stream_get_sid(stream)
 		if sid < 0:
 			return
 
-		print "new sid: ", sid
+		res = self.adb.query_raw("SELECT rating FROM stars WHERE sid=%d" % (sid))
+		if not res.step():
+			n_stars = 0
+		else:
+			n_stars = res.get_value(0, int)
 
-		q = "SELECT rating FROM stars WHERE sid=%d" % (sid)
-		res = self.adb.query_raw(q)
+		self.stars.disconnect_by_func(self.stars_notify_n_stars_cb)
+		self.stars.n_stars = n_stars
+		self.stars.connect('notify::n-stars', self.stars_notify_n_stars_cb)
 
