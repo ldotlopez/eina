fb6a994833dce07e33add513fad5d16117911164
Luis Lopez <luis@cuarentaydos.com>
2011-12-11 18:23:43 +0100

    Stars plugin

--- eina/stars/stars.py
+++ eina/stars/stars.py
@@ -1,8 +1,10 @@
 import os
-from gi.repository import Gtk, Eina, GObject
-from widgets import EinaStars
+from gi.repository import Gtk, GelUI, Eina, GObject
+import widgets
 import gettext
 gettext.textdomain("eina")
+
+class StarsException(Exception): pass
 
 class StarsPlugin(GObject.Object, Eina.Activatable):
 	__gtype_name__ = 'EinaStarsPlugin'
@@ -28,8 +30,8 @@ class StarsPlugin(GObject.Object, Eina.Activatable):
 	def do_activate(self, app):
 		self.adb = app.get_adb()
 		self.lomo = app.get_lomo()
-		self.lomo.connect('change', self.lomo_change_cb)
 
+		# Create or update schema
 		curr_schema_version = self.adb.schema_get_version('stars')
 		updates = (self.schema_version_0,)
 		for i in xrange(curr_schema_version + 1, len(updates)):
@@ -38,24 +40,76 @@ class StarsPlugin(GObject.Object, Eina.Activatable):
 				self.adb.schema_set_version('stars', i)
 			else:
 				print "Update %d fail" % i
+				return False
 
-		box = app.get_player().get_plugins_area()
+		self.lomo.connect('change', self.lomo_change_cb)
 
+		# Create and attach star widget
 		p = os.path.dirname(__file__)
-		self.stars = EinaStars(
+		self.stars = widgets.Stars(
+			hexpand = False,
+			xalign = 0.0,
 			on_image = os.path.join(p, "star-on.svg"),
 			off_image = os.path.join(p, "star-off.svg"))
-		self.stars.set_vexpand(True)
-		self.stars.set_hexpand(False)
 		self.stars.connect('notify::n-stars', self.stars_notify_n_stars_cb)
 		self.stars.show_all()
+
+		box = app.get_player().get_plugins_area()
 		box.pack_start(self.stars, True, True, 0)
+
+		# Setup and attach dock
+		self.dock_widget = widgets.Dock(app)
+		self.dock_widget.show_all()
+
+		dock = app.get_dock()
+		self.dock_tab = dock.add_widget("stars", self.dock_widget, None, 0)
+
+		# Actions
+		self.dock_widget.connect('action-activate-with-amount', self.dock_action_activate_with_mount_cb)
+
 		return True
 
 	def do_deactivate(self, app):
+		self.lomo.disconnect_by_func(self.lomo_change_cb)
+
 		box = app.get_player().get_plugins_area()
-		box.remove(self.grid)
+		box.remove(self.stars)
+
+		dock = app.get_dock()
+		dock.remove_widget(self.dock_tab)
+
+		self.stars = None
+		self.dock_widget = None
+		self.dock_tab = None
+
 		return True
+
+	def dock_action_activate_with_mount_cb(self, w, action, amount):
+		name = action.get_name()
+		if name == 'star-play-action':
+			q = "SELECT sid,uri FROM stars JOIN streams USING(sid) WHERE rating >= %d ORDER BY RANDOM()" % amount
+		elif name == 'top-rated-play-action':
+			q = """
+				SELECT a.sid,uri,rating,b.count FROM
+					(select sid,rating from stars) AS a
+				INNER JOIN
+					(SELECT count(*) as count,sid FROM recent_plays GROUP BY (sid)) AS b
+				USING (sid)
+				INNER JOIN streams USING(sid)
+				ORDER BY a.rating DESC
+				LIMIT %d
+			""" % (amount)
+		elif name == 'random-play-action':
+			q = "SELECT sid,uri FROM streams WHERE sid NOT IN (SELECT sid FROM stars) ORDER BY RANDOM() LIMIT %d" % amount
+		else:
+			raise StarsException('Unknow action: %s' % name)
+
+		uris = []
+		res = self.adb.query_raw(q)
+		while res.step():
+			uris.append(res.get_value(1, str))
+		self.lomo.clear()
+		self.lomo.insert_strv(uris, 0)
 
 	def stars_notify_n_stars_cb(self, w, prop_spec):
 		stream = self.lomo.get_nth_stream(self.lomo.get_current())
@@ -63,7 +117,10 @@ class StarsPlugin(GObject.Object, Eina.Activatable):
 			return
 
 		data = { 'sid' : self.adb.lomo_stream_get_sid(stream), 'rating' : self.stars.n_stars }
-		self.adb.query_exec_raw("INSERT OR REPLACE INTO stars VALUES(%(sid)d, %(rating)d)" % data)
+		if (data['rating'] == 0):
+			self.adb.query_exec_raw("DELETE FROM stars WHERE sid = %(sid)d" % data)
+		else:
+			self.adb.query_exec_raw("INSERT OR REPLACE INTO stars VALUES(%(sid)d, %(rating)d)" % data)
 
 	def lomo_change_cb(self, lomo, f, t):
 		stream = self.lomo.get_nth_stream(t)
