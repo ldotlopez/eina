6371c5633cbee9fe3a27d3ed02b61a7b4bc02475
Luis López <luis@cuarentaydos.com>
2012-05-16 19:23:16 +0200

    Import ancient patch for importer plugin

--- eina/importer/importer.py
+++ eina/importer/importer.py
@@ -1,12 +1,12 @@
-import os, stat, sys, urllib, pyinotify
+import os, stat, sys, urllib
 from gi.repository import GObject, Lomo, Eina, Gtk
 
 ui_mng_str = """
 <ui>
   <menubar name='Main' >
-    <menu name='Tools' action='plugins-menu' >
-      <menuitem name='Bulk import' action='bulk-import-action' />
-    </menu>
+	<menu name='Tools' action='plugins-menu' >
+	  <menuitem name='Bulk import' action='bulk-import-action' />
+	</menu>
   </menubar>
 </ui>
 """
@@ -15,22 +15,30 @@ class Importer:
 	EXTS = "mp3 ogg wav wma".split(" ")
 
 	def __init__(self, adb, *args, **kwargs):
+		self._streams = set()
 		self._adb = adb
+		self._total = 0
+		self._done  = 0
 		self._parser = Lomo.MetadataParser()
 		self._parser.connect('all-tags', self._on_all_tags)
 		if kwargs.has_key('extensions'):
 			Importer.EXTS = kwargs['extensions']
 
 	def _on_all_tags(self, parser, stream):
-		print "== Tags for %s" % os.path.basename(stream.get_tag('uri'))
+		# print "== Tags for %s" % os.path.basename(stream.get_tag('uri'))
+		self._streams.remove(stream)
 		tags = stream.get_tags()
+		self._done = self._done + 1
+		print "%d de %d" % (self._done, self._total)
+
 		if (len(tags) == 1) and (tags[0] == 'uri'):
 			return
 
-
 		self._adb.lomo_stream_attach_sid(stream)
 		sid = self._adb.lomo_stream_get_sid(stream)
-		print "   SID: %d" %sid
+
+		return
+
 		self._adb.query_exec_raw("BEGIN TRANSACTION")
 		for tag in stream.get_tags():
 			try:
@@ -50,7 +58,8 @@ class Importer:
 		for path in paths:
 			try:
 				s = os.lstat(path)
-			except OSError:
+			except OSError as e:
+				print repr(e)
 				continue
 
 			if stat.S_ISLNK(s.st_mode):
@@ -60,7 +69,10 @@ class Importer:
 				self.scan([os.path.join(path, child) for child in os.listdir(path) if child[0] != '.' ], False)
 
 			elif stat.S_ISREG(s.st_mode) and (os.path.splitext(path)[1][1:].lower() in Importer.EXTS):
-				self._parser.parse(Lomo.Stream(uri = 'file://' + urllib.pathname2url(path)), Lomo.MetadataParserPrio.DEFAULT)
+				stream = Lomo.Stream(uri = 'file://' + urllib.pathname2url(path))
+				self._total = self._total + 1
+				self._streams.add(stream)
+				self._parser.parse(stream, Lomo.MetadataParserPrio.DEFAULT)
 
 class ImporterPlugin(GObject.Object, Eina.Activatable):
 	__gtype_name__ = 'EinaImporterPlugin'
@@ -73,18 +85,18 @@ class ImporterPlugin(GObject.Object, Eina.Activatable):
 	def do_activate(self, app):
 		self._app = app
 
-                self._action = Gtk.Action(name = 'bulk-import-action',
+		self._action = Gtk.Action(name = 'bulk-import-action',
 			label = _(u'Bulk import'),
 			tooltip = _(u"Import lots of file"),
 			stock_id = None)
-                self._action.connect('activate', self._on_action_activate)
+		self._action.connect('activate', self._on_action_activate)
 
 		ag = self._app.get_window_action_group()
-                ag.add_action(self._action)
+		ag.add_action(self._action)
 
-                ui_mng = self._app.get_window_ui_manager()
-                self._ui_merge_id = ui_mng.add_ui_from_string(ui_mng_str)
-                ui_mng.ensure_update()
+		ui_mng = self._app.get_window_ui_manager()
+		self._ui_merge_id = ui_mng.add_ui_from_string(ui_mng_str)
+		ui_mng.ensure_update()
 
 		return True
 
@@ -96,8 +108,8 @@ class ImporterPlugin(GObject.Object, Eina.Activatable):
 
 	def _on_action_activate(self, action):
 		dialog = Gtk.FileChooserDialog(title = _(u'Choose the directory to import'),
-			parent = self._app.get_window(),
-			action = Gtk.FileChooserAction.SELECT_FOLDER)
+		parent = self._app.get_window(),
+		action = Gtk.FileChooserAction.SELECT_FOLDER)
 		dialog.add_button(Gtk.STOCK_OK, 1)
 
 		code = dialog.run()
